4f8540bf0c8408d21ae855cdcfd825320d6938a4
funzeros <984512789@qq.com>
2020-01-27 17:22:06 +0800

    首次上船

---
+++ app/src/components/bg/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="bg">
+        <img :src="showBg.src" :alt="showBg.alt">
+    </div>
+</template>
+<script>
+import bg1 from '@/assets/bg/BG1.jpg';
+
+export default {
+    name:"bg",
+    data() {
+        return {
+            img:[
+                {id:"3001",src:bg1,alt:"背景1-沙滩"},
+                {id:"3002",src:"",alt:""}
+            ],
+            bgNum:0,
+        }
+    },
+    computed: {
+        showBg(){
+            return this.img[this.bgNum];
+        }
+    },
+}
+</script>
+<style scoped>
+    .bg{
+
+    }
+</style>
---
+++ app/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <BG/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BG from '@/components/bg/index.vue'
+
+export default {
+  name: 'home',
+  components: {
+    BG
+  }
+}
+</script>
